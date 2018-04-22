4041cfbb3f30828597cb6004e7543580f7f3bf59
zhuwj <774623096@qq.com>
2018-04-22 22:01:42 +0800

    文档修改

--- service-component/service-web/src/main/resources/static/config/prod.env.js
+++ service-component/service-web/src/main/resources/static/config/prod.env.js
@@ -1,8 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://fco2.scdf.org.cn/api"',
-  /*BASE_API: '"http://14.17.66.218:8505"',*/
- /* BASE_API: '"http://e987c11f.ngrok.io"',*/
+  BASE_API: '"127.0.0.1"',
 
 }
