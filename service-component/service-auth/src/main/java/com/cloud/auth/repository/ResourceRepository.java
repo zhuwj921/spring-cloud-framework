8ce2ea08cc03a97a6f15ba6ecd36b53875fc327a
zhuwj <774623096@qq.com>
2020-10-09 17:19:21 +0800

    权限功能

---
+++ service-component/service-auth/src/main/java/com/cloud/auth/repository/ResourceRepository.java
@@ -0,0 +1,15 @@
+package com.cloud.auth.repository;
+
+import com.cloud.auth.domain.Resource;
+import com.cloud.common.base.BaseRepository;
+
+/**
+ * Description: 角色
+ *
+ * @author zhuwj
+ * @version V1.0
+ * @date 2020-10-09
+ */
+
+public interface ResourceRepository extends BaseRepository<Resource> {
+}
---
+++ service-component/service-auth/src/main/java/com/cloud/auth/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.cloud.auth.repository;
+
+import com.cloud.auth.domain.Role;
+import com.cloud.common.base.BaseRepository;
+
+/**
+ * Description: 角色
+ *
+ * @author zhuwj
+ * @version V1.0
+ * @date 2020-10-09
+ */
+
+public interface RoleRepository extends BaseRepository<Role> {
+}
