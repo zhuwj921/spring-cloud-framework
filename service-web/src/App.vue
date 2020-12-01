449229fe4f481adc732bd5db0a575012b816df5e
zhuwj <774623096@qq.com>
2020-12-01 17:55:09 +0800

    创建service-web

--- service-web/src/App.vue
+++ service-web/src/App.vue
@@ -26,11 +26,6 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
