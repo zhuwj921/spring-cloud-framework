e394f0774cf7e47713b180987d47f6004e68e556
zhuwj <774623096@qq.com>
2020-12-14 16:25:43 +0800

    组件服务更改依赖

--- service-common/src/main/java/com/cloud/common/auth/TokenProvider.java
+++ service-common/src/main/java/com/cloud/common/auth/TokenProvider.java
@@ -1,16 +1,11 @@
 package com.cloud.common.auth;
 
 import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.StrUtil;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.cloud.common.constant.GlobalConstant;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
-import java.util.Map;
 
 /**
  * @author: zhuwj
@@ -19,30 +14,13 @@ import java.util.Map;
  */
 public class TokenProvider {
 
-    private static final int EXPIRES_AT = 120;//分钟
+    private static final int EXPIRES_AT = 120;
     private static final String AUDIENCE = "web";
     private static final String KEY = "JldM90LCIQNZ3cQXrER3Jqyq3bkXttLNNVaTsc1TaEPQ9p1gZM8Z0TtvZL88S0bb";
-    /**
-     * 获取token
-     *
-     * @param request
-     * @return
-     */
-    public static String getToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(GlobalConstant.HEADER);
-        if (StrUtil.isBlank(bearerToken)) {
-            Map<String, String[]> queryParams = request.getParameterMap();
-            String[] bearerTokens = queryParams.get(GlobalConstant.TOKEN_NAME);
-            if (ArrayUtil.isNotEmpty(bearerTokens)) {
-                bearerToken = bearerTokens[0];
-            }
-        }
-        String accessToken = resolveToken(bearerToken);
-        return accessToken;
-    }
+
 
     /**
-     * 创建token 使用jwt方式 可以
+     * 创建token 使用jwt方式
      *
      * @return
      */
@@ -57,16 +35,4 @@ public class TokenProvider {
                 .sign(Algorithm.HMAC256(KEY));
     }
 
-    /**
-     * 获取去除前缀的token
-     *
-     * @param bearerToken
-     * @return
-     */
-    private static String resolveToken(String bearerToken) {
-        if (StrUtil.isNotBlank(bearerToken) && bearerToken.startsWith(GlobalConstant.TOKEN_PREFIX)) {
-            return StrUtil.removePrefix(bearerToken, GlobalConstant.TOKEN_PREFIX).trim();
-        }
-        return null;
-    }
 }
