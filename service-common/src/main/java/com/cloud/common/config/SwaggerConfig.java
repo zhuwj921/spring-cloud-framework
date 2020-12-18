505991c45746472a44444c6a2249b0cd50975e9a
zhuwj <774623096@qq.com>
2020-12-18 18:11:38 +0800

    组件服务更改依赖

--- service-common/src/main/java/com/cloud/common/config/SwaggerConfig.java
+++ service-common/src/main/java/com/cloud/common/config/SwaggerConfig.java
@@ -1,23 +1,26 @@
 package com.cloud.common.config;
 
+import com.cloud.common.constant.GlobalConstant;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
- * Description: TODO
+ * Description: SwaggerConfig 配置信息
  *
  * @author zhuwj
  * @version V1.0
  * @date 2020-12-17
  */
-@EnableSwagger2
 @Configuration
 public class SwaggerConfig {
     @Bean
@@ -28,12 +31,33 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.cloud"))
                 .paths(PathSelectors.any())
                 .build()
-                ;
+                .securityContexts(securityContext())
+                .securitySchemes(securitySchemes());
     }
+
+    private List<SecurityScheme> securitySchemes() {
+        return Collections.singletonList(new ApiKey(GlobalConstant.TOKEN_NAME, GlobalConstant.HEADER, "header"));
+    }
+
+    private List<SecurityContext> securityContext() {
+        SecurityContext securityContext = SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+        return Collections.singletonList(securityContext);
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference(GlobalConstant.TOKEN_NAME, authorizationScopes));
+    }
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring Security JWT Guide")
+                .title("Spring Cloud Framework API")
                 .build();
     }
 }
